05da5b164abcdd21065273b3b069e2a4e569a182
Karitchi <karichikarichikarichi@gmail.com>
2025-04-21 16:31:25 +0200

    add food to le-switch page

---
+++ src/lib/components/Food.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Sandwich from "$lib/assets/food/food.jpg?enhanced";
+</script>
+
+<div
+  class="border border-gray-100 shadow-xl overflow-hidden rounded-2xl p-5 w-full flex flex-col gap-5 hover:scale-105 hover:shadow-yellow-500 transition ease-in-out duration-200"
+>
+  <enhanced:img
+    src={Sandwich}
+    alt="Sandwich"
+    class="object-cover rounded-2xl"
+  />
+  <div class="space-y-5">
+    <h3 class="text-2xl">Le Switch</h3>
+    <p>Jambon de parme, mozzarella, pesto, tomates, roquette.</p>
+    <p>€ 5</p>
+    <button class="rounded-full bg-yellow-500 size-5 cursor-pointer">
+      +
+    </button>
+  </div>
+</div>
--- src/routes/restaurants/le-switch/+page.svelte
+++ src/routes/restaurants/le-switch/+page.svelte
@@ -1,1 +1,10 @@
-le switch
+<script>
+  import Food from "$lib/components/Food.svelte";
+</script>
+
+<div class="flex gap-10">
+  <Food />
+  <Food />
+  <Food />
+  <Food />
+</div>
