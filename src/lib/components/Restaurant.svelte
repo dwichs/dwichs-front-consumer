651ba3603131efdac53977d70ea22a37799bf2dc
Karitchi <karichikarichikarichi@gmail.com>
2025-04-29 21:07:33 +0200

    add dynamic `/restaurants` endpoint

--- src/lib/components/Restaurant.svelte
+++ src/lib/components/Restaurant.svelte
@@ -1,17 +1,15 @@
 <script>
   import Logo from "$lib/assets/restaurants/le-switch-logo.png?enhanced";
+
+  let { restaurant } = $props();
 </script>
 
 <a
-  href="/restaurants/le-switch"
+  href={`/restaurants/${restaurant.id}/${restaurant.name}`}
   class="border border-gray-100 shadow-xl overflow-hidden rounded-2xl p-5 w-full flex flex-col gap-5 hover:scale-105 transition ease-in-out duration-200"
 >
   <enhanced:img src={Logo} alt="Restaurant logo" class="object-cover" />
   <div class="space-y-5">
-    <h3 class="text-2xl">Le Switch</h3>
-    <div class="flex gap-5">
-      <p>4,1</p>
-      <span class="material-symbols-outlined"> star </span>
-    </div>
+    <h3 class="text-2xl">{restaurant.name}</h3>
   </div>
 </a>
