54fe57a53ce19ff105acdc2b682ee5c37aaa4742
Karitchi <karichikarichikarichi@gmail.com>
2025-04-29 21:04:42 +0200

    delete test files

--- src/routes/test/+page.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-  const res = await fetch(`/api`);
-  const hello = await res.text();
-
-  return { hello };
-}
--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  /** @type {import('./$types').PageProps} */
-  let { data } = $props();
-  console.log(data.hello);
-</script>
-
-<h1>hello</h1>
-<p>{data.hello}</p>
