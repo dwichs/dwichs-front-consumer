41ce95adda0684bc75e25e0bae780915bfecde82
Karitchi <karichikarichikarichi@gmail.com>
2025-08-28 13:03:07 +0200

    fix restaurants display

--- src/routes/restaurants/+page.ts
+++ src/routes/restaurants/+page.ts
@@ -2,7 +2,6 @@ export const ssr = false;
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
   const response = await fetch(`http://localhost:3000/restaurants`);
-  console.log(response);
   const restaurants = await response.json();
 
   return { restaurants };
