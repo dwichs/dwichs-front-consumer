5340606da1cec934afadbec686ebf94804c551aa
Karitchi <karichikarichikarichi@gmail.com>
2025-08-30 18:48:03 +0200

    add group dropdown menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,14 @@
 <script>
   import Title from "./header/Title.svelte";
   import UserMenuTrigger from "./header/UserMenuTrigger.svelte";
+  import Group from "./header/Group.svelte";
 </script>
 
 <div class="flex justify-between pt-10 items-center">
   <div></div>
   <Title />
-  <UserMenuTrigger />
+  <div class="flex gap-10 justify-center align-center">
+    <Group />
+    <UserMenuTrigger />
+  </div>
 </div>
