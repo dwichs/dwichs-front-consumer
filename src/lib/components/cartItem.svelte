3ff3fd41fb09fcecab84dd6c6d7dd5b1c2a24d07
Karitchi <karichikarichikarichi@gmail.com>
2025-08-31 19:40:17 +0200

    update cart style

--- src/lib/components/cartItem.svelte
+++ src/lib/components/cartItem.svelte
@@ -1,22 +1,26 @@
 <script>
   import Sandwich from "$lib/assets/food/food.jpg?enhanced";
-
   let { menuItem } = $props();
 </script>
 
-<div
-  class="border border-gray-500 flex p-5 rounded-xl gap-5 hover:shadow-xl transition"
->
-  <div class="w-1/8">
-    <enhanced:img
-      src={Sandwich}
-      alt="Sandwich"
-      class="object-cover rounded-xl"
-    />
-  </div>
-  <div>
-    <h1>{menuItem.name}</h1>
-    <p>{menuItem.ingredients}</p>
-    <p>{menuItem.price}</p>
+<div class="border-b border-gray-200 pb-4">
+  <div class="flex gap-4">
+    <div class="w-20 h-20 flex-shrink-0">
+      <enhanced:img
+        src={Sandwich}
+        alt={menuItem.name}
+        class="w-full h-full object-cover rounded-lg"
+      />
+    </div>
+    <div class="flex-1">
+      <div class="flex justify-between items-start">
+        <h2 class="text-lg text-gray-900">{menuItem.name}</h2>
+        <span class="text-lg">${menuItem.price}</span>
+      </div>
+      <p class="text-sm text-gray-600">{menuItem.ingredients}</p>
+      {#if menuItem.addedBy}
+        <p class="text-sm text-gray-600">Added by {menuItem.addedBy.name}</p>
+      {/if}
+    </div>
   </div>
 </div>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,20 +1,18 @@
 <script>
   import { PUBLIC_API_BASE_CLIENT } from "$env/static/public";
-
   let { data } = $props();
-
+  const total = data.cartItems.reduce((sum, item) => {
+    return sum + parseFloat(item.price);
+  }, 0);
   import CartItem from "$lib/components/cartItem.svelte";
-
   async function handleAddOrder() {
     const res = await fetch(`${PUBLIC_API_BASE_CLIENT}/carts`, {
       method: "POST",
       credentials: "include",
     });
-
     if (!res.ok) {
       return;
     }
-
     window.location.href = `/thanks/`;
   }
 </script>
@@ -23,8 +21,11 @@
   {#each data.cartItems as menuItem}
     <CartItem {menuItem} />
   {/each}
+  <div class="flex w-full justify-end">
+    <span class="text-xl">Total: {total}</span>
+  </div>
   <button
-    class=" text-3xl bg-yellow-500 transition ease-in-out rounded-full p-3 hover:shadow-xl border w-full cursor-pointer"
+    class="text-3xl bg-yellow-500 transition ease-in-out rounded-full p-3 mb-5 hover:shadow-xl border w-full cursor-pointer"
     onclick={handleAddOrder}
   >
     Commander
