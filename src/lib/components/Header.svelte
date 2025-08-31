7a3ac53539dfb9623ae6edf3bd02ba1581b6cd9b
Karitchi <karichikarichikarichi@gmail.com>
2025-08-31 02:03:12 +0200

    display current group

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,7 @@
   import Title from "./header/Title.svelte";
   import UserMenuTrigger from "./header/UserMenuTrigger.svelte";
   import GroupDropdown from "./header/GroupDropdown.svelte";
+  import CurrentGroup from "./header/CurrentGroup.svelte";
 
   let { userGroups } = $props();
 </script>
@@ -9,6 +10,7 @@
 <div class="flex justify-center p-5">
   <Title />
   <div class="absolute flex gap-5 top-0 right-0 p-5">
+    <CurrentGroup />
     <GroupDropdown {userGroups} />
     <UserMenuTrigger />
   </div>
---
+++ src/lib/components/header/CurrentGroup.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { currentGroup } from "../../stores/currentGroup.js";
+</script>
+
+<div class="p-3 rounded border border-gray-300 text-gray-700 shadow">
+  Current group: {$currentGroup}
+</div>
