693993e1d49e5126a36f0d5a732b2758048cc548
Karitchi <karichikarichikarichi@gmail.com>
2025-09-02 00:42:00 +0200

    make restaurants responsive

--- src/routes/restaurants/+page.svelte
+++ src/routes/restaurants/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   import Restaurant from "$lib/components/Restaurant.svelte";
   /** @type {import('./$types').PageProps} */
-
   let { data } = $props();
 </script>
 
-<div class="grid grid-cols-4 gap-10">
+<div
+  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-10 p-4 sm:p-0"
+>
   {#each data.restaurants.data as restaurant}
     <Restaurant {restaurant} />
   {/each}
