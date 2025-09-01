45bc8975671e540a04d6637cf6ee93c8b750a64e
Karitchi <karichikarichikarichi@gmail.com>
2025-09-01 11:40:28 +0200

    odd order history anchor

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,16 +4,18 @@
   import GroupDropdown from "./header/GroupDropdown.svelte";
   import CurrentGroup from "./header/CurrentGroup.svelte";
   import Cart from "./header/Cart.svelte";
+  import OrderHistory from "./header/OrderHistory.svelte";
 
   let { userGroups } = $props();
 </script>
 
 <div class="flex justify-center p-5">
   <Title />
-  <div class="absolute flex gap-5 top-0 right-0 p-5">
+  <div class="absolute flex gap-5 top-0 right-0 p-5 text-gray-800">
     <CurrentGroup />
     <GroupDropdown {userGroups} />
     <Cart />
+    <OrderHistory />
     <UserMenuTrigger />
   </div>
 </div>
