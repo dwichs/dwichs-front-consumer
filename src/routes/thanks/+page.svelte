cee35124592fb0e09a601463800872cc3380b5c3
Karitchi <karichikarichikarichi@gmail.com>
2025-09-02 01:05:00 +0200

    make thanks responsive

--- src/routes/thanks/+page.svelte
+++ src/routes/thanks/+page.svelte
@@ -1,6 +1,10 @@
-<div class="space-y-20 w-full text-center">
-  <h1 class="text-center text-7xl underline underline-offset-20">
-    Merci pour votre commande !
+<div class="space-y-8 lg:space-y-20 w-full text-center px-4">
+  <h1
+    class="text-center text-3xl sm:text-4xl lg:text-7xl underline underline-offset-8 lg:underline-offset-20"
+  >
+    Thank you for your order!
   </h1>
-  <p class="italic text-5xl">Nous vous préviendrons dès qu'elle sera prête.</p>
+  <p class="italic text-xl sm:text-2xl lg:text-5xl">
+    We'll notify you as soon as it's ready.
+  </p>
 </div>
