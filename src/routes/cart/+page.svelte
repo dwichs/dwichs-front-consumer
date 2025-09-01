bdaf79f95bf820585ffba0ca1726c8520b7284f4
Karitchi <karichikarichikarichi@gmail.com>
2025-09-02 00:33:26 +0200

    make cart reactive

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -4,19 +4,16 @@
   import { PUBLIC_API_BASE_CLIENT } from "$env/static/public";
   import CartItem from "$lib/components/cartItem.svelte";
   let { data } = $props();
-
   const total = data.cartItems
     .reduce((sum, item) => {
       return sum + parseFloat(item.price);
     }, 0)
     .toFixed(2);
-
   async function handleAddOrder() {
     const currentGroupValue = get(currentGroup);
     const url = currentGroupValue
       ? `${PUBLIC_API_BASE_CLIENT}/orders?groupId=${currentGroupValue}`
       : `${PUBLIC_API_BASE_CLIENT}/orders`;
-
     const res = await fetch(url, {
       method: "POST",
       credentials: "include",
@@ -28,17 +25,15 @@
   }
 </script>
 
-<div class="space-y-5">
+<div class="space-y-5 px-4 sm:px-0">
   {#each data.cartItems as cartItem}
     <CartItem {cartItem} />
   {/each}
-
   <div class="flex w-full justify-end">
-    <span class="text-xl">Total: {total}</span>
+    <span class="text-lg sm:text-xl">Total: {total}</span>
   </div>
-
   <button
-    class="text-3xl bg-yellow-500 transition ease-in-out rounded-full p-3 mb-5 hover:shadow-xl border w-full cursor-pointer"
+    class="text-2xl sm:text-3xl bg-yellow-500 transition ease-in-out rounded-full p-3 mb-5 hover:shadow-xl border w-full cursor-pointer"
     onclick={handleAddOrder}
   >
     Order
