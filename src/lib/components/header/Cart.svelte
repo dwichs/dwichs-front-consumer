8a4f2796ef8bb51fdc40b34a6bce4c55c7a85298
Karitchi <karichikarichikarichi@gmail.com>
2025-09-01 12:01:12 +0200

    various header updates

--- src/lib/components/header/Cart.svelte
+++ src/lib/components/header/Cart.svelte
@@ -1,6 +1,7 @@
 <a
   href="/cart"
-  class="p-3 rounded border border-gray-300 shadow flex cursor-pointer"
+  class="p-3 rounded border border-gray-300 shadow flex items-center justify-between gap-2 cursor-pointer"
 >
   <span class="material-symbols-outlined"> shopping_cart </span>
+  <span>Cart</span>
 </a>
